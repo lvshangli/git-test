66b604c5d6deeed55bf19f44241a2dd82fc86f57
qiulin <qiulin@163.com>
2021-08-15 21:22:06 +0800

    把本地工程添加到Git

---
+++ git-test-project/src/com/jiang/git/FirstGit.java
@@ -0,0 +1,9 @@
+package com.jiang.git;
+
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
