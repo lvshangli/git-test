fdbd4bc54edad16ea05f98eda77a9d70b0636f63
qiulin <qiulin@163.com>
2021-08-15 23:07:45 +0800

    修改FirstGit类

--- First-git-project/src/com/jiang/git/FirstGit.java
+++ First-git-project/src/com/jiang/git/FirstGit.java
@@ -7,9 +7,11 @@ package com.jiang.git;
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("hello git project");
+        //hello test
     }
 
     public void test1(){
         System.out.println("test1");
+        System.out.println("hahah");
     }
 }
