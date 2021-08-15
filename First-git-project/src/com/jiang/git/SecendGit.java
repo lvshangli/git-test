92993da848fc29979149b07d2f551d95037c83f9
qiulin <qiulin@163.com>
2021-08-15 23:34:47 +0800

    添加了一个Second类

---
+++ First-git-project/src/com/jiang/git/SecendGit.java
@@ -0,0 +1,11 @@
+package com.jiang.git;
+
+/**
+ * @Author Shangli
+ * @Date 2021/8/15-23:32
+ */
+public class SecendGit {
+    public static void main(String[] args) {
+        System.out.println("second git hello");
+    }
+}
